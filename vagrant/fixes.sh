e20ef327e2c00cbb88124b56460af05ad596d836
Ahmad Alkilani <aalkilani@users.noreply.github.com>
2016-11-15 22:43:53 -0800

    Update fixes.sh

--- vagrant/fixes.sh
+++ vagrant/fixes.sh
@@ -1,8 +1,2 @@
-# fixes to /pluralsight/stayalive.sh
-cat > /pluralsight/stayalive.sh << EOF
-  if docker ps -a | grep -q 'Exited'; then
-    docker ps -a | grep 'Exited' | awk '{print \$1}' | xargs -I container docker restart container
-  fi
-EOF
-chmod 755 /pluralsight/stayalive.sh
+# fixes go here
 
