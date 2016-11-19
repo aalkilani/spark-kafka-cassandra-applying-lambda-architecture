dae2965d436cab5d5f88084513490da162b288c1
Ahmad Alkilani <aalkilani@users.noreply.github.com>
2016-11-18 20:14:14 -0800

    Zeppling docker run SPARK_HOME
    Zeppling SPARK_HOME incorrectly set to /pluralsight/spark so it's binding to an incorrect version of Spark.

--- vagrant/fixes.sh
+++ vagrant/fixes.sh
@@ -1,2 +1,6 @@
 # fixes go here
 
+# fix Zeppelin docker startup
+docker rm -f zeppelin
+docker run -d --name zeppelin -P --net=host -v /pluralsight:/pluralsight -v /vagrant:/vagrant --env YARN_CONF_DIR=/pluralsight/hadoop_conf --env HADOOP_CONF_DIR=/pluralsight/hadoop_conf --env MASTER=local[*] aalkilani/zeppelin
+
