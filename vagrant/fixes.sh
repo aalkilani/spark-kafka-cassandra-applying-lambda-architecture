2a40eca65903ccfb980b1d5780b5f739a38d6276
Ahmad Alkilani <aalkilani@users.noreply.github.com>
2016-11-18 20:23:02 -0800

    Update fixes.sh

--- vagrant/fixes.sh
+++ vagrant/fixes.sh
@@ -1,6 +1,6 @@
 # fixes go here
 
 # fix Zeppelin docker startup
-docker rm -f zeppelin
+docker ps -a | grep 'zeppelin' | awk '{print $1}' | xargs -I container docker rm -f container
 docker run -d --name zeppelin -P --net=host -v /pluralsight:/pluralsight -v /vagrant:/vagrant --env YARN_CONF_DIR=/pluralsight/hadoop_conf --env HADOOP_CONF_DIR=/pluralsight/hadoop_conf --env MASTER=local[*] aalkilani/zeppelin
 
